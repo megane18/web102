820ed864670fc1a7c511dcf33ab67c7198e0ebde
megane18 <meganealexis12@gmail.com>
2024-09-22 20:54:40 -0400

    Set up react and add all the tags

---
+++ Tropical/src/components/Card.jsx
@@ -0,0 +1,39 @@
+
+
+const Card = () => {
+
+    return (
+        <div id="root">
+            <div class="App">
+                <div class="Header">
+                    <img src="/awning.png"/>
+                    <h1>Food Truck Favorites</h1>
+                    </div>
+                    <div class="container">
+                        <div class="Card">
+                            <img class="truck" src="https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"/>
+                            <h3>Birria-Landia</h3>
+                            <h4>Mexican</h4>
+                            <a href="https://thebirrialandia.com/menu/">
+                            <button>View Menu</button></a></div><div class="Card">
+                                <img class="truck" src="https://res.cloudinary.com/the-infatuation/image/upload/c_fill,w_1200,ar_4:3,g_auto,f_auto/cms/reviews/mysttik-masala/banners/1561989798.42"/>
+                                <h3>Mysttik Masala</h3><h4>Indian</h4><a href="https://indianfoodny.com/">
+                                <button>View Menu</button></a></div><div class="Card">
+                                    <img class="truck" src="https://nypost.com/wp-content/uploads/sites/2/2014/08/081114featureshalaguysbz-1.jpg?quality=75&amp;strip=all&amp;w=1024"/>
+                                    <h3>The Halal Guys</h3><h4>Middle Eastern</h4><a href="https://thehalalguys.com/menu/"><button>View Menu</button></a></div><div class="Card">
+                                        <img class="truck" src="https://nydosas.com/wp-content/uploads/2018/07/maxresdefault-2-1024x630.jpg"/><
+                                            h3>NY Dosas</h3><h4>Vegetarian</h4><a href="https://nydosas.com/menu/"><button>View Menu</button></a></div><div class="Card">
+                                                <img class="truck" src="https://infatuation.imgix.net/media/images/reviews/jerk-pan/banners/1561989308.8.jpg"/>
+                                                <h3>Jerk Pan</h3><h4>Jamaican</h4><a href="https://www.allmenus.com/ny/new-york/360888-jerk-pan/menu/">
+                                                <button>View Menu</button></a></div><div class="Card">
+                                                    <img class="truck" src="https://static01.nyt.com/images/2019/09/04/dining/29hungry-tong2/merlin_159654336_783be212-7726-485e-9250-4793a743af41-jumbo.jpg"/>
+                                                    
+                                                  </div>
+</div>
+</div>
+</div>
+    )
+}
+
+
+export default Card;
---
+++ hello.py
@@ -0,0 +1,4 @@
+def say_hello(name):
+    print(f"Hi {name}, you're beautiful")
+
+say_hello("Megane")
