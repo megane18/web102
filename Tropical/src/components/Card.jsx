a844867404d019384814410e062de78c9e73ef8f
megane18 <meganealexis12@gmail.com>
2024-09-23 20:54:34 -0400

    - Implemented the TropicalFusionTruck component to showcase a selection of Haitian dishes.

--- Tropical/src/components/Card.jsx
+++ Tropical/src/components/Card.jsx
@@ -1,39 +1,97 @@
+import React from 'react';
 
+const TropicalFusionTruck = () => {
+  return (
+    <div className="App">
+      <div className="Header">
+        <img src="/assets/haiti-moulin-sur-mer.webp" alt="Tropical beach" className="header-image" />
+      </div>
+      <h1 className="title">Tropical Fusion Truck</h1>
+      <div className="container">
+        <FoodCard
+          image="/assets/beef-tassot.webp"
+          title="Tassot with plantains, pikliz, and avocado"
+          cuisine="Haitian"
+          link="https://www.savorythoughts.com/haitian-tasso"
+        />
+        <FoodCard
+          image="/assets/12DJONDJONrex-jtlb-superJumbo.jpg"
+          title="Diri Djon Djon"
+          cuisine="Haitian"
+          link="https://www.blackfoodie.co/recipe/djon-djon-haitian-mushroom-rice/"
+        />
+        <FoodCard
+          image="/assets/bon-griot.webp"
+          title="Griot"
+          cuisine="Haitian"
+           link="https://cooking.nytimes.com/recipes/1017276-haitian-pork-griot"
+        />
+        
+         <FoodCard
+          image="/assets/haitian-mac-and-cheese-9960.jpg"
+          title="Mac and Cheese"
+          cuisine="Haitian"
+          link="https://travelfoodatlas.com/makawoni-au-graten-haitian-mac-and-cheese-recipe"
+        />
+         <FoodCard
+          image="/assets/patties.jpg"
+          title="Pate"
+          cuisine="Haitian"
+          link="http://www.suzonspice.com/patteacute.html"
+        />
+         <FoodCard
+          image="/assets/dous-makos.jpg"
+          title="Dous Makos"
+          cuisine="Haitian"
+          link="https://haitiancooking.com/recipe/dous-makos-haitian-fudge/"
+        />
+         <FoodCard
+          image="/assets/beignets.jpg"
+          title="Benyen"
+          cuisine="Haitian"
+          link="https://loveforhaitianfood.com/recipe/haitian-benyen-beignets-de-carnaval/?v=0b3b97fa6688"
+        />
+         <FoodCard
+          image="/assets/fresco.jpg"
+          title="Fresko"
+          cuisine="Haitian"
+          link="https://tchakayiti.com/home/haitian-fresco-lakay/"
+        />
+         <FoodCard
+          image="/assets/ju-sitron.jpg"
+          title="Ju Sitron"
+          cuisine="Haitian"
+          link="https://www.savorythoughts.com/jus-de-citron-haitian-lemonade/"
+        />
+         <FoodCard
+          image="/assets/couronnes.jpg"
+          title="Couronne"
+          cuisine="Haitian"
+          link="https://haiprodistribution.com/product/cola-couronne-fruit-champage/"
+        />
+      </div>
+    </div>
+  );
+};
 
-const Card = () => {
+const FoodCard = ({ image, title, cuisine, link }) => {
+  return (
+    <div className="food-card">
+        <div className='image-container'>
+      <img src={image} alt={title} className="food-image" />
+      </div>
+      <h3>{title}</h3>
+      <h4>{cuisine}</h4>
+      <button className="more-button" onClick={() => window.location.href = link}>More</button>
+    </div>
+  );
+};
 
-    return (
-        <div id="root">
-            <div class="App">
-                <div class="Header">
-                    <img src="/awning.png"/>
-                    <h1>Food Truck Favorites</h1>
-                    </div>
-                    <div class="container">
-                        <div class="Card">
-                            <img class="truck" src="https://pyxis.nymag.com/v1/imgs/38c/2ac/b02a093f6b846ebd1aca78b0d59c7e427a-birria-1.rsocial.w1200.jpg"/>
-                            <h3>Birria-Landia</h3>
-                            <h4>Mexican</h4>
-                            <a href="https://thebirrialandia.com/menu/">
-                            <button>View Menu</button></a></div><div class="Card">
-                                <img class="truck" src="https://res.cloudinary.com/the-infatuation/image/upload/c_fill,w_1200,ar_4:3,g_auto,f_auto/cms/reviews/mysttik-masala/banners/1561989798.42"/>
-                                <h3>Mysttik Masala</h3><h4>Indian</h4><a href="https://indianfoodny.com/">
-                                <button>View Menu</button></a></div><div class="Card">
-                                    <img class="truck" src="https://nypost.com/wp-content/uploads/sites/2/2014/08/081114featureshalaguysbz-1.jpg?quality=75&amp;strip=all&amp;w=1024"/>
-                                    <h3>The Halal Guys</h3><h4>Middle Eastern</h4><a href="https://thehalalguys.com/menu/"><button>View Menu</button></a></div><div class="Card">
-                                        <img class="truck" src="https://nydosas.com/wp-content/uploads/2018/07/maxresdefault-2-1024x630.jpg"/><
-                                            h3>NY Dosas</h3><h4>Vegetarian</h4><a href="https://nydosas.com/menu/"><button>View Menu</button></a></div><div class="Card">
-                                                <img class="truck" src="https://infatuation.imgix.net/media/images/reviews/jerk-pan/banners/1561989308.8.jpg"/>
-                                                <h3>Jerk Pan</h3><h4>Jamaican</h4><a href="https://www.allmenus.com/ny/new-york/360888-jerk-pan/menu/">
-                                                <button>View Menu</button></a></div><div class="Card">
-                                                    <img class="truck" src="https://static01.nyt.com/images/2019/09/04/dining/29hungry-tong2/merlin_159654336_783be212-7726-485e-9250-4793a743af41-jumbo.jpg"/>
-                                                    
-                                                  </div>
-</div>
-</div>
-</div>
-    )
-}
+export default TropicalFusionTruck;
+/* feat: Add Tropical Fusion Truck component with food card listings
 
-
-export default Card;
+- Implemented the TropicalFusionTruck component to showcase a selection of Haitian dishes.
+- Each dish is represented by a FoodCard component, which includes an image, title, cuisine type, and a button linking to the recipe.
+- Added links to various Haitian recipes to provide users with easy access to cooking instructions.
+- Included images for each dish to enhance the visual appeal of the application.
+ */ 
